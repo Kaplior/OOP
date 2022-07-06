b6c1ddaafbe014892735d592ba41817b8d96e736
Nur <83111124+Kaplior@users.noreply.github.com>
2022-07-06 12:53:30 +0500

    code files
    code files were added

---
+++ ConsoleApp9/Inheritance.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Human
+    {
+        private string name;
+        private int age;
+
+        public void print(string name, int age)
+        {
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+        }
+    }
+
+    class Tom : Human
+    {
+        private string job;
+        public void print(string name, int age, string job)
+        {
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+            Console.WriteLine(job);
+        }
+    }
+
+    class MainPro
+    {
+        public static void Main(String[] args)
+        {
+            string name = "Lol";
+            string job = "IT";
+            int age = 5;
+            Tom h = new Tom();
+            h.print(name, age, job);
+            Console.ReadLine();
+        }
+    }
+}
